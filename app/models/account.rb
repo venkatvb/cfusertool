c8e56d3d6456fc4fd96fc08b4774323218399b28
venkatvb <venkateshbabu95@gmail.com>
2015-01-01 23:14:43 +0530

    production fault added accessible

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,5 @@
 class Account < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation, :remember_token
   has_secure_password	#This one made the magic for you :) google it :)
 
   before_save { |account| account.email = email.downcase }
