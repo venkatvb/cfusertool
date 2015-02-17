71863836659ad59415acdb945680688aded3c650
venkatvb <venkateshbabu95@gmail.com>
2015-02-17 10:23:57 +0530

    Friend Listing

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,7 +3,10 @@ class FriendsController < ApplicationController
 	def new
 		if signed_in?
 			@friend = Friend.new
+			@myfriend = Friend.all
+			@index = 1
 		else
+			flash[:danger] = "Oh snap! You should Sign in now! then try again.";
 			render 'users/index'	
 		end
 	end
@@ -15,12 +18,19 @@ class FriendsController < ApplicationController
 		if signed_in?
 			@friend = Friend.new(params[:friends])
 			@friend.account_id = get_account_id
+			@myfriend
+			@index = 1
 		  	if @friend.save
 		  		#saved successful
+		  		@myfriend = Friend.all
+		  		flash[:success] = "yay! #{@myfriend.last.handle} is added."
+		  		render 'new'
 		  	else
+		  		flash[:danger] = "Oh something went wrong! please try again later.";
 		  		render 'new'
 		  	end
 		else
+			flash[:danger] = "Oh snap! You should Sign in now! then try again.";
 			render 'new'
 		end
 	end
