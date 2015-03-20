3e1a9f201f7827520f7d4ebb8281059be925d6a4
venkatvb <venkateshbabu95@gmail.com>
2015-03-20 15:40:12 +0530

    problems classified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,22 +47,29 @@ class UsersController < ApplicationController
 		return problems
 	end
 
-	def getNotDone(sub1, sub2)
+	def getNotDone(sub1, sub2, condition)
 		problems1 = getUniqueProblems(sub1)
 		problems2 = getUniqueProblems(sub2)
-		return problems2 - problems1
+		if condition == :true
+			return problems2 - problems1
+		end 
+		return problems1 & problems2
 	end
 
 	def work
 		base_url = "http://codeforces.com/api/user.status?handle="
+		@firstUser = params[:handle1]
+		@secondUser = params[:handle2]
 		@one = params[:handle1]
 		@two = params[:handle2]
 		url_one = base_url + @one;
 		url_two = base_url + @two;
 		handle1 = parse(url_one)		#uncomment this in production
 		handle2 = parse(url_two)		#uncomment this in production
-		#handle1 = parse("http://localhost/test/venkatvb.html")		#comment this during production
-		#handle2 = parse("http://localhost/test/karthikkamal.html")	#comment this during production
-		@bnota = getNotDone(handle1["result"], handle2["result"])
+		# handle1 = parse("http://localhost/test/venkatvb.html")		#comment this during production
+		# handle2 = parse("http://localhost/test/karthikkamal.html")	#comment this during production
+		@bnota = getNotDone(handle1["result"], handle2["result"], :true)
+		@anotb = getNotDone(handle2["result"], handle1["result"], :true)
+		@aandb = getNotDone(handle2["result"], handle1["result"], :false)
 	end
 end
