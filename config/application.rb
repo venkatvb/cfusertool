80a9b9c0df4f338678f1fa15f39ba54a5d122dad
venkatvb <venkateshbabu95@gmail.com>
2016-04-12 00:53:30 +0530

    cors requests are accepted

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,9 @@ module Cfusertool
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+	config.action_dispatch.default_headers.merge!({
+		      'Access-Control-Allow-Origin' => '*',
+		            'Access-Control-Request-Method' => '*' 
+			        })
   end
 end
