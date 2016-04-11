d17d9bc36102922cebacc64c215dafe6f08aecb3
venkatvb <venkateshbabu95@gmail.com>
2016-04-12 01:04:22 +0530

    rails 3 cors requests accepted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
+	after_filter :set_access_control_headers
+
+	def set_access_control_headers
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Request-Method'] = '*'
+	end
+
 	include SessionsHelper
 	include ApplicationHelper
 	def getNokogiriDoc (url)
